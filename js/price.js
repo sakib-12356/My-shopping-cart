3faa7c057beb2d3145624b04bd6a1c34e2c757e2
Your Name <you@example.com>
2024-09-13 00:03:06 +0900

    first commit

---
+++ js/price.js
@@ -0,0 +1,7 @@
+document.getElementById("incress-phone-quantity").addEventListener("click",()=>{
+ 
+    const phoneQuantity = document.getElementById("phone-quantity");
+
+    const phoneQuantityValue = phoneQuantity.value;
+    console.log(phoneQuantityValue);
+})
